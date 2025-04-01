7fdeea7a5d7744131e07c3b58a0be66c32400366
ALBCAB03 <albertocm92@hotmail.com>
2025-03-31 22:02:47 -0500

    feat: punto 2 pipeline

---
+++ vars/staticAnalysis.groovy
@@ -0,0 +1,37 @@
+def call (boolean abortPipeline = false) {
+    pipeline{
+        agent any
+        enviroment {
+            SONAR_ENV = "sonar"
+        }
+        stages {
+            stage('Static Analysis') {
+                steps {
+                    script {
+                        echo "Running static analysis..."
+                        timeout(time: 5, unit: ){
+                            sh echo "Running SonarQube analysis..."
+                        }
+                        echo "SonarQube analysis completed."
+                        
+                    }
+                }
+            }
+        }
+        stages {
+            stage('Quality Gate') {
+                steps {
+                    script {
+                        if (abortPipeline) {
+                            echo "Quality gate failed. Aborting pipeline."
+                            error("Quality gate failed.")
+                        } else {
+                            echo "Quality gate passed. Proceeding with the pipeline."
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
