17b67c6cd1b14bc453ef71b2049d83d423d4e1f6
ALBCAB03 <albertocm92@hotmail.com>
2025-04-05 10:50:53 -0500

    pruebas

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,26 +1,15 @@
-@Library('threepoints-sharedlib')
 def call(boolean qualityGateAbort = false, boolean abortPipeline = false) {
-    pipeline {
-        agent any
-        stages {
-            stage('Static Code Analysis') {
-                steps {
-                    script {
-                        try {
-                            echo "Analisis de código estático en curso..."
-                        } catch (Exception e) {
-                            echo "Error: Tiempo de espera excedido en el análisis de código"
-                            if (qualityGateAbort) {
-                                error("Pipeline abortado debido al Quality Gate")
-                            }
-                        }
-
-                        if (abortPipeline) {
-                            error("Pipeline abortado según el parámetro abortPipeline")
-                        }
-                    }
-                }
-            }
+    try {
+        echo "Analisis de código estático en curso..."
+    } catch (Exception e) {
+        echo "Error: Tiempo de espera excedido en el análisis de código"
+        if (qualityGateAbort) {
+            error("Pipeline abortado debido al Quality Gate")
         }
     }
+
+    if (abortPipeline) {
+        error("Pipeline abortado según el parámetro abortPipeline")
+    }
+
 }
