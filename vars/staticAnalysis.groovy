eb793da09df4738f4cb9d23d78e9b286b344f7b9
ALBCAB03 <albertocm92@hotmail.com>
2025-04-05 11:19:15 -0500

    pruebas

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,7 +1,12 @@
-def call() {
+def call(boolean abortPipeline = false) {
     timeout(time: 10, unit: 'MINUTES') {
         withEnv(["sonarenv=simulated_value"]){
             sh 'echo "Static analysis started"'
         }
+    }
+    if (abortPipeline) {
+        error('This pipeline has been aborted due to static analysis failure')
+    } else {
+        echo 'Continue with the pipeline'
     }
 }
